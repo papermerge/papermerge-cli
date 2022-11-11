d3509bf99e6718f6a43f224a8715e484bd766581
Eugen Ciur <eugen@papermerge.com>
2022-11-11 21:05:07 +0100

    minor updates

--- papermerge_cli/rest.py
+++ papermerge_cli/rest.py
@@ -1,11 +1,67 @@
+import os
 import click
 import papermerge_restapi_client
+
 from papermerge_restapi_client.apis.tags import (
     auth_api,
     users_api,
-    nodes_api
+    nodes_api,
+    documents_api
 )
 from papermerge_restapi_client.model.auth_token_request import AuthTokenRequest
+
+
+def get_restapi_client(host, token):
+    configuration = papermerge_restapi_client.Configuration(host=host)
+    configuration.api_key['Token Authentication'] = f'Token {token}'
+
+    return papermerge_restapi_client.ApiClient(configuration)
+
+
+def get_user_home_uuid(restapi_client):
+    api_instance = users_api.UsersApi(restapi_client)
+
+    resp = api_instance.users_me_retrieve()
+    ret = resp.body['data']['relationships']['home_folder']['data']['id']
+
+    return ret
+
+
+def upload_document(restapi_client, parent_uuid, file_path):
+    nodes_api_instance = nodes_api.NodesCreate(restapi_client)
+    title = os.path.basename(file_path)
+
+    body = dict(
+        data=dict(
+            type="documents",
+            attributes=dict(
+                title=title,
+            ),
+            relationships=dict(
+                parent=dict(
+                    data=dict(
+                        type="folders",
+                        id=parent_uuid,
+                    ),
+                ),
+            ),
+        ),
+    )
+    response = nodes_api_instance.nodes_create(body=body)
+    document_uuid = response.body['data']['id']
+    documents_api_instance = documents_api.UploadFile(restapi_client)
+
+    with open(file_path, 'rb') as f:
+        body = f.read()
+
+    path_params = {
+        'file_name': title,
+        'document_id': document_uuid
+    }
+    documents_api_instance.upload_file(
+        body=body,
+        path_params=path_params
+    )
 
 
 def perform_auth(host, username, password):
@@ -109,3 +165,29 @@ def perform_me(
     click.echo(f'email={email}')
     click.echo(f'home folder uuid={home_folder_uuid}')
     click.echo(f'inbox folder uuid={inbox_folder_uuid}')
+
+
+def perform_import(host, token, file_or_folder):
+
+    restapi_client = get_restapi_client(host, token)
+    home_uuid = get_user_home_uuid(restapi_client)
+
+    if os.path.isfile(file_or_folder):
+        click.echo(f"Importing {file_or_folder}...")
+        upload_document(
+            restapi_client=restapi_client,
+            parent_uuid=home_uuid,
+            file_path=file_or_folder
+        )
+        return
+
+    for entry in os.scandir(file_or_folder):
+        if entry.is_file():
+            click.echo(f"Importing {entry.path}...")
+            upload_document(
+                restapi_client=restapi_client,
+                parent_uuid=home_uuid,
+                file_path=entry.path
+            )
+        else:
+            click.echo(f"Skipping {entry.path} as it is not a file")
