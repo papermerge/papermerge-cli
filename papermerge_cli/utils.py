afa44a61ecf2b14551db0195e0066f9c89cf0c91
Eugen Ciur <eugen@papermerge.com>
2022-12-20 11:26:01 +0100

    Fixes exception when host name contains trailing slash(s). Issue#15 (#16)

--- papermerge_cli/utils.py
+++ papermerge_cli/utils.py
@@ -1,3 +1,18 @@
 
 def pretty_breadcrumb(path: tuple) -> str:
     return f"/{'/'.join(path)}"
+
+
+def sanitize_host(host: str) -> str:
+    """Remove unnecessary characters from host name
+
+    Unnecessary characters may be one or multiple whitespaces,
+    or one/multiple slashes at the end of the host name.
+    """
+    host = host.strip()
+    if host[-1] == '/':
+        clean_host = host[0:-1]
+        # just in case there are more trailing slash characters...
+        return sanitize_host(clean_host)
+
+    return host
