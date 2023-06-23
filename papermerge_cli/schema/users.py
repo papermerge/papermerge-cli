f4529847845daefe56ce3c386318171613327e74
Eugen Ciur <eugen@papermerge.com>
2023-06-23 09:34:05 +0200

    use laconic syntax for generating pydantic models(#27)

--- papermerge_cli/schema/users.py
+++ papermerge_cli/schema/users.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class User(BaseModel):
     id: UUID
     username: str
-    email: str
+    email: EmailStr
     created_at: datetime
     updated_at: datetime
     home_folder_id: UUID
