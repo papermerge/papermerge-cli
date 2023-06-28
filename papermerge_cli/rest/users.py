9754833a6a07ff736ee00ef284e2b4f69888bcd7
Eugen Ciur <eugen@papermerge.com>
2023-06-28 07:13:26 +0200

    remove papermerge-restapi-client dependency

--- papermerge_cli/rest/users.py
+++ papermerge_cli/rest/users.py
@@ -1,9 +1,8 @@
 from papermerge_cli.api_client import ApiClient
 from papermerge_cli.schema.users import User
-from papermerge_cli.utils import catch_401, host_required, token_required
+from papermerge_cli.utils import host_required, token_required
 
 
-@catch_401
 @host_required
 @token_required
 def get_me(
